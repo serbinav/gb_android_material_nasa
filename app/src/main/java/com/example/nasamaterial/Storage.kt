5c76c73a10db5856504ea3a8829be310b8a8f803
andrey <a.serbin@litebox.ru>
2021-10-30 22:37:37 +0400

    добавил SharedPreferences

---
+++ app/src/main/java/com/example/nasamaterial/Storage.kt
@@ -0,0 +1,19 @@
+package com.example.nasamaterial
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Storage(context: Context) {
+
+    private val KEY = "KEY_THEME"
+    private var pref: SharedPreferences =
+        context.getSharedPreferences("app_theme", Context.MODE_PRIVATE)
+
+    fun getTheme(): Int {
+        return pref.getInt(KEY, 0)
+    }
+
+    fun setTheme(theme: Int) {
+        pref.edit().putInt(KEY, theme).apply()
+    }
+}
