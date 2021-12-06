876b46af632bce0a1ed29a1c8f378e5c4bdce25c
andrey <a.serbin@litebox.ru>
2021-12-06 21:33:28 +0400

    создание "простого экрана загрузки" через Handler и отдельную activity

---
+++ app/src/main/java/com/example/nasamaterial/ui/SplashActivity.kt
@@ -0,0 +1,39 @@
+package com.example.nasamaterial.ui
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import com.example.nasamaterial.R
+
+class SplashActivity : AppCompatActivity() {
+
+    var handler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        findViewById<AppCompatImageView>(R.id.image_view).animate().rotationBy(360f)
+            .setInterpolator(AccelerateDecelerateInterpolator()).setDuration(3000)
+            .setListener(object : Animator.AnimatorListener {
+
+                override fun onAnimationEnd(animation: Animator?) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+
+                override fun onAnimationRepeat(animation: Animator?) {}
+                override fun onAnimationCancel(animation: Animator?) {}
+                override fun onAnimationStart(animation: Animator?) {}
+            })
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacksAndMessages(null)
+        super.onDestroy()
+    }
+}
